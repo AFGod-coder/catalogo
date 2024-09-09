cc4ece175746475f3352a1f23c39082b354d48da
AFGoder <u20231212923@usco.edu.co>
2024-09-08 19:00:40 -0500

    Firs commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const section = document.querySelector('.grupo-medias.canilleras');
+
+function renderArticle(tipoMedia, cantidad) {
+
+for (let i = 1; i <= cantidad; i++) {
+    const article = document.createElement('article');
+    article.classList.add('media');
+
+    const imgDiv = document.createElement('div');
+    imgDiv.classList.add('imgen');
+
+    const img = document.createElement('img');
+    img.src = `img/${tipoMedia} (${i}).png`;
+    img.alt = '';
+
+    const titleDiv = document.createElement('div');
+    titleDiv.classList.add('tittle');
+
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+
+    if (tipoMedia === 'canillera'){
+        h3.textContent = 'Canillera';
+        p.textContent = '$14.000';
+    } else if (tipoMedia === 'tobillera'){
+        h3.textContent = 'Tobillera';
+        p.textContent = '$12.000';
+    } else if (tipoMedia === 'baleta'){
+        h3.textContent = 'Baleta';
+        p.textContent = '$10.000';
+    }
+
+    
+    
+
+    titleDiv.appendChild(h3);
+    titleDiv.appendChild(p);
+
+    article.appendChild(imgDiv);
+    imgDiv.appendChild(img);
+    article.appendChild(titleDiv);
+
+    section.appendChild(article);
+}
+
+}
+
+renderArticle('canillera', 9);
+renderArticle('tobillera', 1);
+renderArticle('baleta', 1);
+
+
+const whatsappLink = document.querySelector('.whatsapp');
+
+whatsappLink.addEventListener('click', () => {
+    const phoneNumber = '+573204920412';
+    const message = 'Deseo hacer un pedido de medias 🧦🎊';
+
+    window.location.href = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+});
