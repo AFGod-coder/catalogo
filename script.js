7bccff210b13b94938f1e2d64ba51f04370861e9
AFGoder <u20231212923@usco.edu.co>
2024-09-09 10:50:54 -0500

    Se añadieron filtros para tipos de medias

--- script.js
+++ script.js
@@ -24,20 +24,20 @@ for (let i = 1; i <= cantidad; i++) {
         p.textContent = '$22.000';
 
         p.style.color = '#197FE6';
-        
+        article.classList.add('promo');
     } else if (tipoMedia === 'tobillera'){
         h3.textContent = 'Tobillera';
         p.textContent = '$12.000';
+        article.classList.add('tobillera');
     } else if (tipoMedia === 'baleta'){
         h3.textContent = 'Baleta';
         p.textContent = '$10.000';
+        article.classList.add('baleta');
     } else if (tipoMedia === 'canillera'){
         h3.textContent = 'Canillera';
         p.textContent = '$14.000';
+        article.classList.add('canillera');
     }
-
-    
-    
 
     titleDiv.appendChild(h3);
     titleDiv.appendChild(p);
@@ -64,4 +64,59 @@ whatsappLink.addEventListener('click', () => {
     const message = 'Deseo hacer un pedido de medias 🧦🎊';
 
     window.location.href = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+});
+
+
+
+const promoButton = document.querySelector('#promos');
+const canilleraButton = document.querySelector('#canilleras');
+const tobilleraButton = document.querySelector('#tobilleras');
+const baletaButton = document.querySelector('#baletas');
+
+promoButton.addEventListener('click', () => {
+    const mediaArticles = document.querySelectorAll('.media');
+    
+    mediaArticles.forEach(article => {
+        if (article.classList.contains('promo')) {
+            article.style.display = 'block';
+        } else {
+            article.style.display = 'none';
+        }
+    });
+});
+
+canilleraButton.addEventListener('click', () => {
+    const mediaArticles = document.querySelectorAll('.media');
+    
+    mediaArticles.forEach(article => {
+        if (article.classList.contains('canillera')) {
+            article.style.display = 'block';
+        } else {
+            article.style.display = 'none';
+        }
+    });
+});
+
+tobilleraButton.addEventListener('click', () => {
+    const mediaArticles = document.querySelectorAll('.media');
+    
+    mediaArticles.forEach(article => {
+        if (article.classList.contains('tobillera')) {
+            article.style.display = 'block';
+        } else {
+            article.style.display = 'none';
+        }
+    });
+});
+
+baletaButton.addEventListener('click', () => {
+    const mediaArticles = document.querySelectorAll('.media');
+    
+    mediaArticles.forEach(article => {
+        if (article.classList.contains('baleta')) {
+            article.style.display = 'block';
+        } else {
+            article.style.display = 'none';
+        }
+    });
 });
